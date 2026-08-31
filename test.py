131107847ab1f1dc98da32b5fdb967efc051a297
qlovy <qlovy@sysnet.ch>
2026-08-31 21:09:40 +0200

    add storing key body angle + fix get_angle_between function

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 from ultralytics import YOLO
 import cv2
+import numpy as np
 
 # Load the YOLO26 pose model
 model = YOLO("yolo26n-pose.pt")
@@ -32,6 +33,7 @@ def run_keypoint_on_video(video_path, output_path="output_video.mp4", conf=0.5):
 
     frame_count = 0
     dict_result = {}
+    angle_results = {}
     while cap.isOpened():
         ret, frame = cap.read()
         if not ret:
@@ -40,8 +42,16 @@ def run_keypoint_on_video(video_path, output_path="output_video.mp4", conf=0.5):
         results = model(frame, conf=conf, verbose=False)
         if frame_count == 0:
             for _, kps in enumerate(results[0].keypoints.xy):
-                for i, (x, y) in enumerate(kps):
-                    dict_result[f"{KEYPOINT_NAMES[i]}"] = (x, y)
+                for i, t in enumerate(kps):
+                    dict_result[f"{KEYPOINT_NAMES[i]}"] = t.numpy()
+
+
+            angle_results["knee"] = get_angle_between(dict_result["Left Ankle"], dict_result["Left Knee"], dict_result["Left Hip"])
+            angle_results["hip"] = get_angle_between(dict_result["Left Knee"], dict_result["Left Hip"], dict_result["Left Shoulder"])
+            angle_results["elbow"] = get_angle_between(dict_result["Left Shoulder"], dict_result["Left Elbow"], dict_result["Left Wrist"])
+            angle_results["back"] = get_angle_between(dict_result["Left Shoulder"], dict_result["Left Hip"], np.array([dict_result["Left Shoulder"][0], dict_result["Left Hip"][1]]) )
+
+            cv2.imwrite("./img/frame0.jpg", results[0].plot())
 
 
 
@@ -55,6 +65,16 @@ def run_keypoint_on_video(video_path, output_path="output_video.mp4", conf=0.5):
     cap.release()
     out.release()
     print(f"Done! Output saved to: {output_path}")
+    print(angle_results)
 
+
+def get_angle_between(p1, p2, p3):
+    u1 = p1 - p2
+    u2 = p3 - p2
+    dot_prod = np.dot(u1, u2)
+    mag_u1 = np.linalg.norm(u1)
+    mag_u2 = np.linalg.norm(u2)
+    angle = np.degrees( np.arccos( dot_prod / (mag_u1 * mag_u2) ) )
+    return angle
 
 run_keypoint_on_video("./video/IMG_0385.MP4", output_path="./output/output.mp4")
