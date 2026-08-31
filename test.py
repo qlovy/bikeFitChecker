55841a185c319c5241365522da577a0d3e946135
qlovy <qlovy@sysnet.ch>
2026-08-31 22:02:43 +0200

    add measurement of key body part angles and compare result

--- test.py
+++ test.py
@@ -1,3 +1,5 @@
+# sources bike fit body angle: https://www.bikefittr.com/bike-fit-angle-chart
+
 from ultralytics import YOLO
 import cv2
 import numpy as np
@@ -5,9 +7,15 @@ import numpy as np
 # Load the YOLO26 pose model
 model = YOLO("yolo26n-pose.pt")
 
+theory_angle = {
+    "knee": [140, 150],
+    "hip": [45, 60],
+    "elbow": [150, 170],
+    "back": [35, 50]
+}
 
 def run_keypoint_on_video(video_path, output_path="output_video.mp4", conf=0.5):
-    KEYPOINT_NAMES = [
+    keypoint_name = [
         "Nose", "Left Eye", "Right Eye", "Left Ear", "Right Ear",
         "Left Shoulder", "Right Shoulder", "Left Elbow", "Right Elbow",
         "Left Wrist", "Right Wrist", "Left Hip", "Right Hip",
@@ -18,7 +26,7 @@ def run_keypoint_on_video(video_path, output_path="output_video.mp4", conf=0.5):
     cap = cv2.VideoCapture(video_path)
     if not cap.isOpened():
         print(f"Error: Could not open video '{video_path}'")
-        return
+        return None
 
     fps = int(cap.get(cv2.CAP_PROP_FPS))
     width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
@@ -34,26 +42,55 @@ def run_keypoint_on_video(video_path, output_path="output_video.mp4", conf=0.5):
     frame_count = 0
     dict_result = {}
     angle_results = {}
+
+    left_index = {
+        "knee": keypoint_name.index("Left Knee"),
+        "hip": keypoint_name.index("Left Hip"),
+        "shoulder": keypoint_name.index("Left Shoulder"),
+        "elbow": keypoint_name.index("Left Elbow"),
+        "wrist": keypoint_name.index("Left Wrist"),
+        "ankle": keypoint_name.index("Left Ankle")
+    }
+
+    max_knee_angle = 0
+    min_hip_angle = 0
+
     while cap.isOpened():
         ret, frame = cap.read()
         if not ret:
             break
 
         results = model(frame, conf=conf, verbose=False)
+
+        xy_val = results[0].keypoints.xy[0]
+
+        left_xy = {
+            "knee": xy_val[left_index["knee"]].numpy(),
+            "hip": xy_val[left_index["hip"]].numpy(),
+            "shoulder": xy_val[left_index["shoulder"]].numpy(),
+            "elbow": xy_val[left_index["elbow"]].numpy(),
+            "wrist": xy_val[left_index["wrist"]].numpy(),
+            "ankle": xy_val[left_index["ankle"]].numpy()
+        }
+
+        knee_angle = get_angle_between(left_xy["ankle"], left_xy["knee"], left_xy["hip"])
+        hip_angle = get_angle_between(left_xy["knee"], left_xy["hip"], left_xy["shoulder"])
+
         if frame_count == 0:
-            for _, kps in enumerate(results[0].keypoints.xy):
-                for i, t in enumerate(kps):
-                    dict_result[f"{KEYPOINT_NAMES[i]}"] = t.numpy()
+            angle_results = {
+                "knee": knee_angle,
+                "hip": hip_angle,
+                "elbow": get_angle_between(left_xy["shoulder"], left_xy["elbow"], left_xy["wrist"]),
+                "back": get_angle_between(left_xy["shoulder"], left_xy["hip"], np.array([left_xy["shoulder"][0], left_xy["hip"][1]]) )
+            }
+            max_knee_angle = knee_angle
+            min_hip_angle = hip_angle
 
+        # knee angle is measured at the bottom of the pedal stroke, so max value
+        max_knee_angle = knee_angle if (max_knee_angle < knee_angle) else max_knee_angle
 
-            angle_results["knee"] = get_angle_between(dict_result["Left Ankle"], dict_result["Left Knee"], dict_result["Left Hip"])
-            angle_results["hip"] = get_angle_between(dict_result["Left Knee"], dict_result["Left Hip"], dict_result["Left Shoulder"])
-            angle_results["elbow"] = get_angle_between(dict_result["Left Shoulder"], dict_result["Left Elbow"], dict_result["Left Wrist"])
-            angle_results["back"] = get_angle_between(dict_result["Left Shoulder"], dict_result["Left Hip"], np.array([dict_result["Left Shoulder"][0], dict_result["Left Hip"][1]]) )
-
-            cv2.imwrite("./img/frame0.jpg", results[0].plot())
-
-
+        # hip angle is measures at the top of the pedal stroke, so min value
+        min_hip_angle = hip_angle if (min_hip_angle > hip_angle) else min_hip_angle
 
         annotated_frame = results[0].plot()
         out.write(annotated_frame)
@@ -62,10 +99,14 @@ def run_keypoint_on_video(video_path, output_path="output_video.mp4", conf=0.5):
         if frame_count % 30 == 0:
             print(f"  Processed {frame_count}/{total} frames...")
 
+        if frame_count == total:
+            angle_results["knee"] = max_knee_angle
+            angle_results["hip"] = min_hip_angle
+
     cap.release()
     out.release()
     print(f"Done! Output saved to: {output_path}")
-    print(angle_results)
+    return angle_results
 
 
 def get_angle_between(p1, p2, p3):
@@ -77,4 +118,18 @@ def get_angle_between(p1, p2, p3):
     angle = np.degrees( np.arccos( dot_prod / (mag_u1 * mag_u2) ) )
     return angle
 
-run_keypoint_on_video("./video/IMG_0385.MP4", output_path="./output/output.mp4")
+def compare_angle(angle, arr_angle):
+    return arr_angle[0] <= angle <= arr_angle[1]
+
+def print_result(dict1, dict2):
+    print("Results from video analytics: ")
+    for key in dict1:
+        print(f"  {key} angle = {dict1[key]: .1f}   Interval = {dict2[key][0]} - {dict2[key][1]}   Valid = {compare_angle(dict1[key], theory_angle[key])}")
+
+measure_angles = run_keypoint_on_video("./video/IMG_0385.MP4", output_path="./output/output.mp4")
+
+if measure_angles is None:
+    print("Issue with video analytics")
+    exit()
+
+print_result(measure_angles, theory_angle)
